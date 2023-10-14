60ca552bd73a75ab86ec51c5acb930c741824c65
Chrystine Martins <chrystine.martins@outlook.com>
2023-10-14 13:21:14 -0300

    new content

--- components/cardnews.js
+++ components/cardnews.js
@@ -1,10 +1,35 @@
-class cardnews extends HTMLElement {
+class cardnews extends HTMLElement{
     constructor() {
         super();
 
         const shadow = this.attachShadow({ mode: "open" });
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
+
+    build() { 
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card-left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card-right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+
+
+    }
+    
+    styles() {}
+
+
+
+
 }
 
-customElements.define('card-news', cardnews)
+customElements.define("card-news", cardnews);
